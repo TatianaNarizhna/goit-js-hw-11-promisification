b12fa76139063b2e16e63b535d08a9f1c3b1c15a
TatianaNarizhna <narizhna.tania@gmail.com>
2021-05-21 22:28:17 +0300

    Updates

--- src.6c40c264.js
+++ src.6c40c264.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss"),require("./code.js/task1"),require("./code.js/task2"),require("./code.js/task3");
 },{"./sass/main.scss":"clu1","./code.js/task1":"clu1","./code.js/task2":"clu1","./code.js/task3":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-js-hw-11-promisification/src.8adc73df.js.map
+//# sourceMappingURL=/goit-js-hw-11-promisification/src.6c40c264.js.map
